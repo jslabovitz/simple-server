93c0b0b5b919b2f404c2b0117eff4b70ca9f3df1
John Labovitz <johnl@johnlabovitz.com>
2015-03-28 10:37:06 -0400

    Make work.

--- lib/simple-server.rb
+++ lib/simple-server.rb
@@ -3,30 +3,52 @@ require 'addressable/uri'
 require 'path'
 require 'pp'
 
-class SimpleServer < Sinatra::Application
+class SimpleServer < Sinatra::Base
 
   VERSION = '0.1'
 
+  configure do
+    enable :logging
+  end
+
+  before do
+    env['rack.logger'] = Logger.new(STDERR)
+  end
+
+  def path_for_uri(uri)
+    Path.new(settings.root) / 'sites' / uri.host / Path.new(uri.normalized_path).relative_to('/')
+  end
+
+  def normalized_host(host)
+    host ||= settings.default_host
+    host.gsub(/^www\./, '').gsub(/\..*?$/, '')
+  end
+
   get '*' do
-    uri_path = Addressable::URI.parse(params[:splat].first).normalized_path
-    path = Path.new(settings.public_dir) / Path.new(uri_path).relative_to('/')
-    path /= 'index' if uri_path[-1] == '/'
-    if path.directory?
-      log.debug "#{uri_path} => [redirect] #{uri_path}"
-      return redirect "#{uri_path}/"
-    elsif path.file? && path.basename.to_s[0] != '.'
-      log.debug "#{uri_path} => [file] #{path}"
+    uri = Addressable::URI.parse(params[:splat].first)
+    uri.scheme = 'http'
+    uri.host = normalized_host(request.host)
+    uri.path += 'index.html' if uri.normalized_path[-1] == '/'
+    path = path_for_uri(uri)
+    if path.basename.to_s[0] == '.'
+      logger.info "#{uri} => hidden file: denying"
+      halt 403, 'unauthorized'
+    elsif path.directory?
+      logger.info "#{uri} => implicit directory: redirecting"
+      return redirect "#{uri.path}/"
+    elsif path.file?
+      logger.info "#{uri} => direct file: #{path}"
       return send_file(path)
     elsif path.extname.empty?
-      %w{.html .jpg}.each do |extname|
+      %w{.html .htm}.each do |extname|
         p = path.add_extension(extname)
         if p.file?
-          log.debug "#{uri_path} => [file] #{p}"
+          logger.info "#{uri} => guessed extension #{extname}: #{p}"
           return send_file(p)
         end
       end
     end
-    log.debug "#{uri_path} => [not found]"
+    logger.info "#{uri} => not found"
     halt 404, 'not found'
   end
 
