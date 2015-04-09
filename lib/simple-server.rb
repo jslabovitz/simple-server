40513fb8e1769b85cd172c2d879a526a77627907
John Labovitz <johnl@johnlabovitz.com>
2015-04-09 10:27:59 -0400

    Use Addressable::URI's host parsing/normalization instead of our own. Use full domain names (minus www/web prefix) for finding site directories. Trap URI parsing errors. Improve logging.

--- lib/simple-server.rb
+++ lib/simple-server.rb
@@ -16,38 +16,41 @@ class SimpleServer < Sinatra::Base
   end
 
   def path_for_uri(uri)
-    path = Path.new(settings.root) / 'sites' / normalized_host(uri.host) / Path.new(uri.normalized_path).relative_to('/')
+    path = Path.new(settings.root) / 'sites'
+    path /= (uri.normalized_host ||= settings.default_host).sub(/^(www|web).*?\./, '')
+    path /= Path.new(uri.normalized_path).relative_to('/')
     path /= 'index.html' if uri.normalized_path[-1] == '/'
     path
   end
 
-  def normalized_host(host)
-    host ||= settings.default_host
-    host.downcase.gsub(/^www\./, '').gsub(/\..*?$/, '').gsub(/[^a-z0-9.]/, '')
-  end
-
   get '*' do
-    uri = Addressable::URI.parse(request.url)
+    begin
+      uri = Addressable::URI.parse(request.url)
+      uri.host = request.host
+    rescue Addressable::URI::InvalidURIError => e
+      logger.info "#{uri}: invalid URI (#{e.message}): denying"
+      halt 400, 'invalid URI'
+    end
     path = path_for_uri(uri)
     if path.basename.to_s[0] == '.'
-      logger.info "#{uri} => hidden file: denying"
+      logger.info "#{uri} (#{path}): hidden file: denying"
       halt 403, 'unauthorized'
     elsif path.directory?
-      logger.info "#{uri} => implicit directory: redirecting"
+      logger.info "#{uri} (#{path}): implicit directory: redirecting"
       return redirect "#{uri.path}/"
     elsif path.file?
-      logger.info "#{uri} => direct file: #{path}"
+      logger.info "#{uri} (#{path}): direct file: serving"
       return send_file(path)
     elsif path.extname.empty?
       %w{.html .htm}.each do |extname|
         p = path.add_extension(extname)
         if p.file?
-          logger.info "#{uri} => guessed extension #{extname}: #{p}"
+          logger.info "#{uri} (#{path}): guessed extension #{extname}: serving #{p}"
           return send_file(p)
         end
       end
     end
-    logger.info "#{uri} => not found"
+    logger.info "#{uri} (#{path}): not found: denying"
     halt 404, 'not found'
   end
 
