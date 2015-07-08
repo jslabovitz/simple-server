d2963bbb0e0fc6fbd9fc6134dbd78afec298e740
John Labovitz <johnl@johnlabovitz.com>
2015-07-08 09:59:52 -0400

    Move request parsing into separate method. Add redirection. Guess short URIs even if the request URI appears to have an extension (eg, 'foo.10').

--- lib/simple-server.rb
+++ lib/simple-server.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'addressable/uri'
 require 'path'
 require 'pp'
+require 'yaml'
 
 class SimpleServer < Sinatra::Base
 
@@ -24,14 +25,19 @@ class SimpleServer < Sinatra::Base
     path
   end
 
-  get '*' do
+  def parse_request(request)
     begin
       uri = Addressable::URI.parse(request.url)
-      uri.host = request.host
     rescue Addressable::URI::InvalidURIError => e
       logger.info "#{uri}: invalid URI (#{e.message}): denying"
       halt 400, 'invalid URI'
     end
+    uri.host = request.host
+    uri
+  end
+
+  get '*' do
+    uri = parse_request(request)
     path = path_for_uri(uri)
     if path.basename.to_s[0] == '.'
       logger.info "#{uri} (#{path}): hidden file: denying"
@@ -39,10 +45,14 @@ class SimpleServer < Sinatra::Base
     elsif path.directory?
       logger.info "#{uri} (#{path}): implicit directory: redirecting"
       return redirect "#{uri.path}/"
+    elsif (redirect_path = path.add_extension('.redirect')).exist?
+      redirect = YAML.load(redirect_path.read)
+      logger.info "#{uri} (#{path}): redirecting via #{redirect[:code]} to #{redirect[:uri]}"
+      return redirect redirect[:uri], redirect[:code]
     elsif path.file?
       logger.info "#{uri} (#{path}): direct file: serving"
       return send_file(path)
-    elsif path.extname.empty?
+    else
       %w{.html .htm}.each do |extname|
         p = path.add_extension(extname)
         if p.file?
